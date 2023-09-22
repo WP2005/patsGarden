cfbb5057ccc1308d0db6e1c2ef2c1fc1f507cedf
WP2005 <jylee0128@gmail.com>
2023-09-22 10:49:22 -0500

    9.22.2023: Final prototype (jlee)

---
+++ TEST.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <a href="/images/ads.pdf">
+      <i class="fas fa-tags fa-icon-medium fa-coupon-styling"></i>Coupons.
+    </a>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -21,73 +21,93 @@
 			<div id="page-wrapper">
 				<!-- Wrapper -->
 					<div id="wrapper">
-						<!-- Panel (Welcome) -->
+							<!-- Panel (Welcome) -->
 							<section class="panel banner right"  id="first">
 								<div class="content color0 span-3-75">
-									<h1 class="major">
-									Pats Farms <br> Garden Center</h1>
-									<p>Welcome to your neighborhood Garden Center! We are located next to Pats Farms Grocery Store and we have been serving the community for over 20 years. 
-										We offer a wide variety of plants, flowers, and gardening supplies, as well as expert advice from our friendly and knowledgeable staff.</p>							
-									<a href="#" target=_blank class="icon brands icon"> 
-									<i class="fas fa-tags fa-icon-medium"></i>See our latest coupons. 
+								
+								<img src="<?php echo get_theme_file_uri('images/PFGC-logo-1.png') ?>" class="logo" /><hr>
+									
+								
+									<a  href="https://goo.gl/maps/sKPjhTe5H8qWxEns9"  target=_blank class="icon brands icon"> 
+										<i class="fas fa-map-marker-alt  fa-icon-medium fa-map-styling"></i>1126 Merrick Ave.  N. Merrick, NY 11566
 									</a>
-								</div>
-								<div class="image span-1-75" data-position="25% 25%">
-									<img src="<?php echo get_theme_file_uri('images/pic01-b.jpg') ?>" alt="" />
-								</div>
-							</section>
+									<span><i class="fas fa-mobile-alt fa-icon-medium fa-map-styling" ></i> 516-539-2313 </span>
+									
+									<span><i class="far fa-clock fa-icon-medium fa-map-styling fa-clock-styling"></i> Opens:  Mon - Sun: 7am to 7pm </span>
 
-						<!-- Panel (Full Image) -->
+									<a href="https://www.facebook.com/profile.php?id=100088339561403" target=_blank class="icon brands icon"> 
+										<i class="fab fa-facebook fa-icon-medium fa-coupon-styling"></i> Facebook 
+									</a>
+
+									<a href="http://localhost:10016/wp-content/uploads/2023/09/ads.pdf" target=_blank class="icon brands icon"> 
+										<i class="fas fa-ad fa-icon-medium fa-coupon-styling"></i>Weekly Ads 
+									</a>
+									<a href="#Special" target=_blank class="icon brands icon"> 
+										<i class="fas fa-tags fa-icon-small fa-coupon-styling"></i>Fall Inventory 
+									</a>
+									<a href="#Member" target=_blank class="icon brands icon"> 
+										<i class="fas fa-users fa-sm fa-coupon-styling"></i> Membership Rewards
+									</a>
+									<hr>
+
+								</div>
+								
+								
+								<div class="image span-1-75 signage" data-position="25% 25%"></div>
+							</section>
+							<!-- Panel (Full Image) -->
 							<section class="panel">
-								<div class="gallery">
-									<a href="images/gallery/fulls/09.jpg" class="image  span-2-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/04c.jpg') ?>" alt="" /></a>
+								<div class="gallery">			
+									<a href="images/gallery/fulls/09.jpg" class="image  span-2-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pg-1.jpg') ?>" alt="" /></a>
 								</div>
 							</section>
-
-						<!-- Panel (Location) -->
-							<section class="panel spotlight xmedium xright">
-								<div class="content xspan-7">
-									<h2 class="major">Location</h2>
-									<p>
-										1126  Merrick Ave,</br>
-										N. Merrick, NY 11566</br>
-										<a href="https://goo.gl/maps/sKPjhTe5H8qWxEns9" target=_blank> See map </a>
-									</p>
-									<br>Hours: </br>
-									<ul>
-										<li> Mon - Sun: 7am to 7pm </li>
-									</ul>
+							<!-- Panel (About Us) -->
+							<section class="panel spotlight xmedium right">
+								<div class="content span-3-75">
+									<h2 class="major">About Us</h2>
+									<p>Welcome to your neighborhood Garden Center. We are located next to Pat’s Farms Supermarket and we have been serving the community over 38yrs. Here at Pat’s Farms Garden Center, we aim to provide the best quality plants at great prices. We have great selections of Annuals, Perennials, Tropicals, Vegetables, Herbs, Trees and Shrubs. We also provide the customers with seasonal decor all year round. We carry all your gardening needs from fertilizer, soil, pots, gardening tools and so much more.</p>
+									<a href="/images/ads.pdf" target=_blank class="icon brands icon"> 
+										<i class="fas fa-ad fa-icon-medium fa-coupon-styling"></i>Weekly Ads 
+									</a>
+									<a href="#Special" target=_blank class="icon brands icon"> 
+										<i class="fas fa-tags fa-icon-small fa-coupon-styling"></i>Fall Inventory 
+									</a>								
 								</div>
 								<div class="image filtered tinted" data-position="top left">
 									<img src="<?php echo get_theme_file_uri('images/pic02.jpg') ?>" alt="" />
 								</div>
 							</section>
-
-						<!-- Panel (4 image montage) -->
+							<!-- Panel (Image montage) -->
 							<section class="panel">
 								<div class="gallery">
 									<div class="group span-4-5">
-										<a href="images/gallery/fulls/05.jpg" class="image  span-3" data-position="top"><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/05b.jp') ?>g" alt="" /></a>
+										<a href="images/gallery/fulls/05.jpg" class="image  span-3" data-position="top"><img src="<?php echo get_theme_file_uri('images/pfgc-fall-2.png') ?>" alt="" /></a>
 										<a href="images/gallery/fulls/06.jpg" class="image  span-1-5" data-position="center"><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/06a.jp') ?>g" alt="" /></a>
 										<a href="images/gallery/fulls/07.jpg" class="image  span-1-5" data-position="bottom"><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/07a.jpg') ?>" alt="" /></a>
 										<a href="images/gallery/fulls/08.jpg" class="image  span-3" data-position="top"><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/08b.jpg') ?>" alt="" /></a>
 									</div>
 								</div>
 							</section>
-
-						<!-- Panel (Contact) -->
+							<!-- Panel (What we Carry) -->
 							<section class="panel color4-alt">
 								<div class="intro color4">
-									<h2 class="major">Contact</h2>
-									<p>Tel: 516-539-2313</br>
-									Fax: 516-539-2316</br>
-									Email: pats_farms@yahoo.com</p>
-									<ul class="contact-icons color1">
-										<li class="icon brands fa-facebook-f"><a href="https://www.facebook.com/originalpatsfarms" target=_blank>Facebook</a></li>
+									<h2 class="major">What we carry:</h2>
+								
+									<ul>
+										<li>Garden Tools</li>
+										<li>Garden Pots</li>
+										<li>Fertilizers</li>
+										<li>Indoor/Outdoor plants</li>
 									</ul>
+									<a href="http://localhost:10016/wp-content/uploads/2023/09/ads.pdf" target=_blank class="icon brands icon"> 
+										<i class="fas fa-ad fa-icon-medium fa-coupon-styling"></i>Weekly Ads 
+									</a>
+									<a href="#Special" target=_blank class="icon brands icon"> 
+										<i class="fas fa-tags fa-icon-small fa-coupon-styling"></i>Fall Inventory 
+									</a>
 								</div>
 							</section>
-        				<!-- Panel () -->
+        					<!-- Panel (Img Montage) -->
 							<section class="panel">
 								<div class="gallery">
 
@@ -96,19 +116,19 @@
 											href="images/gallery/fulls/01.jpg"
 											class="image xfiltered span-3"
 											data-position="bottom"
-											><img src="<?php echo get_theme_file_uri('images/gallery/IMG_1086.JPG') ?> " alt=""
+											><img src="https://morningchores.com/wp-content/uploads/2019/10/10-10-10-Fertilizer-What-It-Is-and-the-Right-and-Wrong-Way-to-Use-It-FB.jpg " alt=""
 										/></a>
 										<a
 											href="images/gallery/fulls/02.jpg"
 											class="image xfiltered span-1-5"
 											data-position="center"
-											><img src="<?php echo get_theme_file_uri('images/gallery/square-2.jpg') ?> " alt=""
+											><img src="https://images.squarespace-cdn.com/content/v1/60481e8b74b043581a581d36/1668083305479-BFC28HQE5I6ZTQANL565/Untitled+design+%2898%29+%281%29.jpg " alt=""
 										/></a>
 										<a
 											href="images/gallery/fulls/03.jpg"
 											class="image xfiltered span-1-5"
 											data-position="bottom"
-											><img src="<?php echo get_theme_file_uri('images/gallery/square-1.jpg') ?> " alt=""
+											><img src="<?php echo get_theme_file_uri('images/pfgc-fall-4.png') ?> " alt=""
 										/></a>
 									</div>
 									<a
@@ -117,27 +137,93 @@
 									data-position="top"
 									><img src="<?php echo get_theme_file_uri('images/gallery/thumbs/09a.jpg') ?> " alt=""
 									/></a>
+								</div>
+							</section>	
 
-
-								</div>	
-
-								<div class="color0 endpanel">
-									
-									
-									
-									<!--
-										<a href="#first" class="button primary color1 circle icon solid fa-angle-left"></a>
-									<h2 class="major">Grocery Store</h2>
-										<p>
-											Shop at Pat's Farms Grocery Store. Fresh, local produce just steps away!.	
-										</p>
-										<a href="https://originalpatsfarms.com/" target=_blank class="icon brands"> 
-											<i class="far fa-arrow-alt-circle-right"></i> Visit our grocery store.
-										</a>
-									-->
-
+							<!-- Panel (Seasonal) -->
+							<section class="panel color4-alt" id="Special">
+								<div class="intro color1">
+									<h2 class="major">Fall Specials</h2>
+									<h4>Discounts on...</h4>
+									<ul>
+										<li>Most plants, trees, shrubs</li>
+										<li>ground covers, roses</li>
+										<li>opiaries, cactus, palms</li>
+										<li>azaleas & more...</li>
+									</ul>
+									<a href="http://localhost:10016/wp-content/uploads/2023/09/ads.pdf" target=_blank class="icon brands icon"> 
+										<i class="fas fa-ad fa-icon-medium fa-coupon-styling"></i>Weekly Ads 
+									</a>
+								</div>
+							</section>
+							<!-- Panel (Full Image) -->
+							<section class="panel">
+								<div class="gallery">
+									<a href="images/gallery/fulls/09.jpg" class="image  span-1-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pfgc-fall-4.png') ?> " alt="" /></a>
+								</div>
+								<div class="gallery">
+									<a href="images/gallery/fulls/09.jpg" class="image  span-1-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pfgc-fall-3c.png') ?>"  alt="" /></a>
+								</div>
+								<div class="gallery">
+									<a href="images/gallery/fulls/09.jpg" class="image  span-1-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pfgc-fall-10.png') ?> " alt="" /></a>
+								</div>
+								<div class="gallery">
+									<a href="images/gallery/fulls/09.jpg" class="image  span-1-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pfgc-fall-11.png') ?> " alt="" /></a>
+								</div>
+							</section>
+							<!-- Panel (Tricks Tips) -->
+							<section class="panel banner right" >
+								<div class="content color0 span-3-75">
+									<h2 class="major">GARDENING TRICKS & TIPS</h2>
+									<a href="https://www.pennlive.com/gardening/2018/05/vegetable_gardening_isnt_just.html" target=_blank class="icon brands"> 
+										<i class="far fa-arrow-alt-circle-right"></i> Things to Consider When Starting a Vegetable Garden.
+									</a>
+									<a href="https://planthardiness.ars.usda.gov/" target=_blank class="icon brands"> 
+										<i class="far fa-arrow-alt-circle-right"></i> Know your USDA Hardiness Zone.
+									</a>
+									<hr>
+									<a href="http://localhost:10016/wp-content/uploads/2023/09/ads.pdf" target=_blank class="icon brands icon"> 
+										<i class="fas fa-ad fa-icon-medium fa-coupon-styling"></i>Weekly Ads 
+									</a>
+									<a href="#Special" target=_blank class="icon brands icon"> 
+										<i class="fas fa-tags fa-icon-small fa-coupon-styling"></i>Fall Inventory 
+									</a>
 								</div>
 								
+							</section>
+
+							<!-- Panel (Full Image) -->
+							<section class="panel">
+								<div class="gallery">			
+									<a href="images/gallery/fulls/09.jpg" class="image  span-2-5" data-position="right"><img src="<?php echo get_theme_file_uri('images/pic01-d.jpg') ?>" alt="" /></a>
+								</div>
+							</section>
+
+							<!-- Panel (Member) -->
+							<section class="panel color4-alt" id="Member">
+								<div class="intro color1">
+									<h2 class="major">Welcome Rewards</h2>
+									<h4>Enroll today and access exclusive benefits everytime you shop at Pat’s Farms Garden Center.</h4>
+									<ul>
+										<li>10% off your entire purchase*</li>
+										<li>Enroll in person at our store.</li>
+										<li><a href="http://localhost:10016/wp-content/uploads/2023/09/Rewards Flyer.pdf" target=_blank class="icon brands icon"> 
+										See brochure</li>
+									</a>
+									</ul>
+									<br><br>
+									<sub>* Certain restrictions apply</sub>
+								</div>
+							</section>
+
+							<!-- Panel (End Panel) -->
+							<section class="panel banner right">
+								<div class="color0 endpanel">
+
+									<a href="#first" class="icon-left" >Back</a>
+
+									<a href="#first" class="icon-up">Back to top</a>
+								</div>
 							</section>
 					</div>
 			</div>
